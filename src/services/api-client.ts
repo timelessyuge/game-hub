67426a0e33a531e56b24c05616098ee9f8b64c30
timelessyuge <timelessyuge@gmail.com>
2025-01-03 14:38:49 +0800

    Use a Type Assertion for iconMap[platform.slug] - "as={iconMap[platform.slug] as IconType}"

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "a76385dae6b84e84a55c98198839a79d",
   },
 });
